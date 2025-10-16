ec3937c77f74a04f7970dd9b76939b51e78ece05
munsung143 <akunta3475@gmail.com>
2025-10-16 20:08:31 +0900

    [fix] 리팩토링

--- Test_MultiRender/Window.cs
+++ Test_MultiRender/Window.cs
@@ -46,8 +46,7 @@ namespace TK_Texture
         private int[] _vbo;
         private int[] _vao;
         private Shader[] _shader;
-        private Texture _texture;
-        private Texture _texture2;
+        private Texture[] _texture;
         Stopwatch timer;
 
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
@@ -59,46 +58,61 @@ namespace TK_Texture
             _vbo = new int[count];
             _vao = new int[count];
             _shader = new Shader[count];
+            _texture = new Texture[2];
+            for (int i = 0; i < count; i++)
+            {
+                _vao[i] = GL.GenVertexArray();
+                _vbo[i] = GL.GenBuffer();
+            }
+            _ebo = GL.GenBuffer();
+            _shader[0] = new Shader("Shaders/rectshader.vert", "Shaders/rectshader.frag");
+            _shader[1] = new Shader("Shaders/rainbowshader.vert", "Shaders/rainbowshader.frag");
+            _shader[2] = new Shader("Shaders/triangleshader.vert", "Shaders/triangleshader.frag");
+
+            _shader[0].SetInt("texture0", 0);
+            _shader[0].SetInt("texture1", 1);
+
+            int rect_loc_position = _shader[0].GetAttribLocation("aPosition");
+            int rect_loc_texCoord = _shader[0].GetAttribLocation("aTexCoord");
+            int rainbow_loc_position = _shader[1].GetAttribLocation("aPosition");
+            int rainbow_loc_color = _shader[1].GetAttribLocation("aColor");
+            int triangle_loc_position = _shader[2].GetAttribLocation("aPosition");
+
+            _texture[0] = Texture.LoadFromFile("Resources/container.png");
+            _texture[1] = Texture.LoadFromFile("Resources/awesomeface.png");
 
             timer = new Stopwatch();
             timer.Start();
 
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-            _shader[0] = new Shader("Shaders/rectshader.vert", "Shaders/rectshader.frag");
-            _shader[1] = new Shader("Shaders/rainbowshader.vert", "Shaders/rainbowshader.frag");
-            _shader[2] = new Shader("Shaders/triangleshader.vert", "Shaders/triangleshader.frag");
-            _shader[0].SetInt("texture0", 0);
-            _shader[0].SetInt("texture1", 1);
-
-            _texture = Texture.LoadFromFile("Resources/container.png");
-            _texture2 = Texture.LoadFromFile("Resources/awesomeface.png");
-
-
-            // 순서 : VAO생성 및 바인딩 - (셰이더 활성화) - VBO생성 및 바인딩 - (EBO생성 및 바인딩) - VAO어트리뷰트 링크
+            // 순서 : (셰이더 활성화) - VAO바인딩 - VBO바인딩 - (EBO바인딩) - VAO어트리뷰트 링크
             // 사각형
-            _vao[0] = GL.GenVertexArray();
             _shader[0].Use();
             GL.BindVertexArray(_vao[0]);
-            _vbo[0] = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo[0]);
-            GL.BufferData(BufferTarget.ArrayBuffer, _rectangle.Length * sizeof(float), _rectangle, BufferUsageHint.StaticDraw);
-            _ebo = GL.GenBuffer();
+            GL.BufferData(
+                target: BufferTarget.ArrayBuffer,
+                size: _rectangle.Length * sizeof(float),
+                data: _rectangle,
+                usage: BufferUsageHint.StaticDraw);
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _rectIndices.Length * sizeof(uint), _rectIndices, BufferUsageHint.StaticDraw);
-            int vertextLocation = _shader[0].GetAttribLocation("aPosition");
-            GL.EnableVertexAttribArray(vertextLocation);
+            GL.BufferData(
+                target: BufferTarget.ElementArrayBuffer,
+                size: _rectIndices.Length * sizeof(uint),
+                data: _rectIndices,
+                usage: BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(rect_loc_position);
             GL.VertexAttribPointer(
-                index: vertextLocation,
+                index: rect_loc_position,
                 size: 3,
                 type: VertexAttribPointerType.Float,
                 normalized: false,
                 stride: 5 * sizeof(float),
                 offset: 0);
-            int texCoordLocation = GL.GetAttribLocation(_shader[0].Handle, "aTexCoord");
-            GL.EnableVertexAttribArray(texCoordLocation);
+            GL.EnableVertexAttribArray(rect_loc_texCoord);
             GL.VertexAttribPointer(
-                index: texCoordLocation,
+                index: rect_loc_texCoord,
                 size: 2,
                 type: VertexAttribPointerType.Float,
                 normalized: false,
@@ -106,25 +120,25 @@ namespace TK_Texture
                 offset: 3 * sizeof(float));
 
             // 무지개 삼각형
-            _vao[1] = GL.GenVertexArray();
             _shader[1].Use();
             GL.BindVertexArray(_vao[1]);
-            _vbo[1] = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo[1]);
-            GL.BufferData(BufferTarget.ArrayBuffer, _rainbow.Length * sizeof(float), _rainbow, BufferUsageHint.StaticDraw);
-            int vertextLocation2 = _shader[1].GetAttribLocation("aPosition");
-            GL.EnableVertexAttribArray(vertextLocation2);
+            GL.BufferData(
+                target: BufferTarget.ArrayBuffer,
+                size: _rainbow.Length * sizeof(float),
+                data: _rainbow,
+                usage: BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(rainbow_loc_position);
             GL.VertexAttribPointer(
-                index: vertextLocation2,
+                index: rainbow_loc_position,
                 size: 3,
                 type: VertexAttribPointerType.Float,
                 normalized: false,
                 stride: 6 * sizeof(float),
                 offset: 0);
-            int location = _shader[1].GetAttribLocation("aColor");
-            GL.EnableVertexAttribArray(location);
+            GL.EnableVertexAttribArray(rainbow_loc_color);
             GL.VertexAttribPointer(
-                index: location,
+                index: rainbow_loc_color,
                 size: 3,
                 type: VertexAttribPointerType.Float,
                 normalized: false,
@@ -132,16 +146,17 @@ namespace TK_Texture
                 offset: 3 * sizeof(float));
 
             //일반삼각형
-            _vao[2] = GL.GenVertexArray();
             _shader[2].Use();
             GL.BindVertexArray(_vao[2]);
-            _vbo[2] = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo[2]);
-            GL.BufferData(BufferTarget.ArrayBuffer, _triangle.Length * sizeof(float), _triangle, BufferUsageHint.StaticDraw);
-            int location2 = _shader[2].GetAttribLocation("aPosition");
-            GL.EnableVertexAttribArray(location2);
+            GL.BufferData(
+                target: BufferTarget.ArrayBuffer,
+                size: _triangle.Length * sizeof(float),
+                data: _triangle,
+                usage: BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(triangle_loc_position);
             GL.VertexAttribPointer(
-                index: location2,
+                index: triangle_loc_position,
                 size: 3,
                 type: VertexAttribPointerType.Float,
                 normalized: false,
@@ -149,8 +164,8 @@ namespace TK_Texture
                 offset: 0);
 
             _shader[0].Use();
-            _texture.Use(TextureUnit.Texture0);
-            _texture2.Use(TextureUnit.Texture1);
+            _texture[0].Use(TextureUnit.Texture0);
+            _texture[1].Use(TextureUnit.Texture1);
             GL.BindVertexArray(_vao[0]);
 
 
@@ -162,8 +177,8 @@ namespace TK_Texture
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
             _shader[0].Use();
-            _texture.Use(TextureUnit.Texture0);
-            _texture2.Use(TextureUnit.Texture1);
+            _texture[0].Use(TextureUnit.Texture0);
+            _texture[1].Use(TextureUnit.Texture1);
             GL.BindVertexArray(_vao[0]);
             GL.DrawElements(PrimitiveType.Triangles, _rectIndices.Length, DrawElementsType.UnsignedInt, 0);
 
@@ -172,7 +187,7 @@ namespace TK_Texture
             GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
             double timeValue = timer.Elapsed.TotalSeconds;
-            float greenValue = (float)Math.Sin(timeValue) / 2.0f + 0.5f;
+            float greenValue =  (float)Math.Sin(timeValue) / 2.0f + 0.5f;
             _shader[2].SetVector4("ourColor", new Vector4(0.0f, greenValue, 0.0f, 1.0f));
             _shader[2].Use();
             GL.BindVertexArray(_vao[2]);
